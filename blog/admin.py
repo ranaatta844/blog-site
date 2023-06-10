8305a64c9e8b96987d03a36166ba0cc03daae258
ranaatta844 <ranaatta844@gmail.com>
2023-06-10 15:06:14 +0500

    Creating the models for the blog-site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Tag, Post, Comment, AuthorProfile
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields={'slug':('title',)}
+  list_display = ('slug','published_at')
 
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+admin.site.register(AuthorProfile)
